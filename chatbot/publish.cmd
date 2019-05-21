5d525b9f676f8c0ac9b776cadbcf6b8dfcb488c1
colinsteffen <SteffenColin@t-online.de>
2019-05-21 17:56:38 +0200

    Deleted old Botfiles

--- chatbot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=sundnchatbot-Web-Deploy.pubxml -p:Password=gubdER9pS2rAbjlgLvTibFMxqzfhsHLb4F7uvXm4sClxr7Zc3wWweX0MG5py
-
--- faq-webappbot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=faq-webappbot-Web-Deploy.pubxml -p:Password=GPokrRhdHdltduokqglF7B6Cml8qQEegsmjfgb3nXNJQKJKJx1k5Mr5E378k
-
