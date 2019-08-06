de7f3468ebda66425d95baa7bb43f415cb5edad0
colinsteffen <SteffenColin@t-online.de>
2019-08-06 13:56:35 +0200

    Added Event Class

---
+++ faq-botservice/Model/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoBot.Model
+{
+    public class Event
+    {
+        public String Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public PlaceOfStudy Place { get; set; }
+        public String TargetGroup { get; set; }
+        public String Organizer { get; set; }
+        public String Registration { get; set; }
+    }
+}
